2a0b73e2bd72d9908c336382f8a176ab258659ea
Dan <neujmin1@gmail.com>
2022-01-02 16:01:17 -0700

    added to utils.py 'imagepre' and 'findenemies'. Will need scipy installed

--- utils.py
+++ utils.py
@@ -1,6 +1,15 @@
 import cv2
 import numpy as np
+from scipy import stats
 import matplotlib.pyplot as plt
+
+
+class Enemy:
+
+    def __init__(self, color, location):
+        self.color = color
+        self.location = location
+        self.score = 0
 
 
 def imageprep(screen_capture):
@@ -52,14 +61,138 @@ def imageprep(screen_capture):
     return img, dot_count
 
 
+def findenemies(img):
+    '''
+    first run imageprep to ensure color border is removed
+    :param img:
+    :return:
+    '''
+
+    def isgray(pixel):
+        return pixel[0] == pixel[1] == pixel[2]
+
+    # we know the enemies are colored pixels
+    R = img[:, :, 0].astype(int)
+    G = img[:, :, 1].astype(int)
+    B = img[:, :, 2].astype(int)
+
+    M = (R + G + B) / 3
+    M = M.astype(int)
+
+    # pixel coordinates of all enemies
+    ii_enemies = np.argwhere(M != R)
+
+    # return now if ii_enemies is empty
+    if not ii_enemies.any():
+        return [], []
+
+    # encircle and gray out first enemy cluster you find
+    i = ii_enemies[0]
+
+    # walk left until you reach an all-gray pixel
+    while True:
+        i[0] -= 1
+        if isgray(img[i[0], i[1]]):
+            break
+
+    # walk back right once, back into color
+    i[0] += 1
+
+    darkest_gray = 255
+
+    # initial director. Known, since we chose to initially walk left
+    director = -1 + 0j
+
+    enemy_contour = []
+
+    keep_going = True
+    while keep_going:
+        for t in np.arange(0, 360, 45):
+            vec = director*np.exp(1j*t*np.pi/180)
+            x = int(np.round(vec.real))
+            y = int(np.round(vec.imag))
+            pixel = img[i[0]-y, i[1]+x]
+            if isgray(pixel):
+                if pixel[0] < darkest_gray:
+                    darkest_gray = pixel[0]
+            else:
+                # update director and i
+                vec *= -1
+                vec *= np.exp(1j*np.pi/4)
+                director = vec
+                #print(f'({i[1]+x}, {i[0]-y})')  # for debug
+                #img[i[0]-y, i[1]+x, :] = [0, 255, 0]  # for debug
+                enemy_contour.append([i[0]-y, i[1]+x])
+                i = [i[0]-y, i[1]+x]
+                if i[0] == ii_enemies[0][0] and i[1] == ii_enemies[0][1]:
+                    keep_going = False
+                break
+
+    # convert enemy_contours into opencv-compatible object
+    enemy_contour = np.array([[[p[1], p[0]]] for p in enemy_contour]).astype(np.int32)
+
+    # get all pixel coords of this enemy
+    mask = np.zeros(img.shape[0:2], np.uint8)
+    cv2.drawContours(mask, [enemy_contour], 0, 255, -1)
+    pixel_coords = np.transpose(np.nonzero(mask))
+
+    # determine this enemy's color
+    xx = [coord[0] for coord in pixel_coords]
+    yy = [coord[1] for coord in pixel_coords]
+    sub_img = img[xx, yy, :]
+
+    reds = []
+    greens = []
+    blues = []
+    for p in sub_img:
+        reds.append(p[0])
+        greens.append(p[1])
+        blues.append(p[2])
+
+    red = stats.mode(reds)[0][0]
+    green = stats.mode(greens)[0][0]
+    blue = stats.mode(blues)[0][0]
+
+    enemy = Enemy([red, green, blue], [int(round(np.mean(xx))), int(round(np.mean(yy)))])
+
+    # now that we have found an enemy...remove it
+    cv2.drawContours(img, [enemy_contour], -1, (int(darkest_gray), int(darkest_gray), int(darkest_gray)), -1)
+
+    return img, enemy
+
+
 if __name__ == '__main__':
 
     # read in the raw, screen-capture image
-    #img = cv2.imread('IMG_1157.PNG')
-    img = cv2.imread('image0.png')
+    img = cv2.imread('IMG_1157.PNG')
+    #img = cv2.imread('image0.png')
+
+    img_orig = img.copy()
 
     img, dot_count = imageprep(img)
 
-    plt.imshow(img)
-    plt.title(f'player dots = {dot_count}')
+    out_img = img.copy()
+    enemies = []
+    while True:
+        img, enemy = findenemies(img)
+        if enemy:
+            enemies.append(enemy)
+            out_img = img.copy()
+        else:
+            break
+
+    print(f'\nplayer dots: {dot_count}')
+    print('list of enemies:')
+    for (q, enemy) in enumerate(enemies):
+        print(f'\tEnemy {q+1} has RGB color ({enemy.color[0]}, {enemy.color[1]}, {enemy.color[2]})'
+              f' at prepared position ({enemy.location[1]}, {enemy.location[0]})')
+
+    plt.figure(1)
+    plt.imshow(out_img)
+    plt.title('prepared stage')
+
+    plt.figure(2)
+    plt.imshow(img_orig)
+    plt.title('raw stage')
+
     plt.show()
